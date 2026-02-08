09a1ee571298879133d1c52712d0363601986bae
Arnotto <Arnotto1002@hotmail.com>
2026-02-08 15:02:13 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -35,7 +35,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  //   HERO_RECOMMEND_COVER: 'https://raw.githubusercontent.com/Arnotto/picture/refs/heads/essay/bigw.webp', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
@@ -117,10 +117,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '握爪爪',
+  HEO_SOCIAL_CARD_TITLE_2: '留下你的气味标记',
+  HEO_SOCIAL_CARD_TITLE_3: '看看都在说什么',
+  HEO_SOCIAL_CARD_URL: 'https://irradiated-moonflower-15e.notion.site/30103232579a80b8aa1cfe13fc3aa1bf?source=copy_link',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
